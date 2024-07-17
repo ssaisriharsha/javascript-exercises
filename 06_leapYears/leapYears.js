41492c4cebafb247bc3f093d04f5528de364ee71
Harsha <saisri2946@gmail.com>
2024-07-17 23:03:01 +0530

    Completed problem 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!(year % 4)) {
+        if (!(year % 400)) {
+            return true;
+        }
+        else if (!(year % 100)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
