8659230493e371d219d6601c17750955bddef625
Harsha <saisri2946@gmail.com>
2024-07-17 23:14:12 +0530

    Completed problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = parseFloat(((temp - 32)*5/9).toFixed(1));
+  return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let newTemp = parseFloat((temp*9/5 + 32).toFixed(1));
+  return newTemp;
 };
 
 // Do not edit below this line
