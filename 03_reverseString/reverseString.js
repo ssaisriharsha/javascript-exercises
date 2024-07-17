bdf3df1e67fb4de005727636b920b0718b066c8d
Harsha <saisri2946@gmail.com>
2024-07-17 22:09:57 +0530

    Completed problems 2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newstr = "";
+    for (let i = -1; i >= -(string.length); i--) {
+        newstr += string.at(i);
+    }
+    return newstr;
 };
 
 // Do not edit below this line
