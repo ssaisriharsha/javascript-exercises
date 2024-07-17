76aff7c2d7e743f21fb92c57f38eb785e6a677be
Harsha <saisri2946@gmail.com>
2024-07-17 22:40:47 +0530

    Completed problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+    let len = array.length
+    for (let num of nums) {
+        for(let i = 0; i < len; i++) {
+            if (array.at(i) === num) {
+                array.splice(i, 1);
+                i--;
+                len--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
