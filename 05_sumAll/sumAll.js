c2c8b5a0d40b4fa3ffa5a0e9580c7d28907b7588
Harsha <saisri2946@gmail.com>
2024-07-17 22:55:32 +0530

    Completed problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+    const sumAll = function(...nums) {
+        let sum = 0;
+        nums.sort();
+        if (Math.floor(nums[0]) != Math.ceil(nums[0]) || Math.floor(nums[1]) != Math.ceil(nums[1]) || nums[0] < 0 || nums[1] < 0 || typeof nums[0] != "number" || typeof nums[1] != "number") {
+            return "ERROR";
+        }
+        for (let i = nums[0]; i <= nums[1]; i++) {
+            sum += i;
+        }
+        return sum;
+    };
 
-};
-
-// Do not edit below this line
-module.exports = sumAll;
+    // Do not edit below this line
+    module.exports = sumAll;
